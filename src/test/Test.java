efa3ca5faca42187ecdcf4800cbd9b56e59c09bd
Unknown <svezyridis@gmail.com>
2018-05-17 00:29:09 -0700

    INITIAL COMMIT

---
+++ src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+import authapi.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(authapi.Authapi.GetToken("SYSTEM", "savvas", "savvas"));
+
+	}
+
+}
